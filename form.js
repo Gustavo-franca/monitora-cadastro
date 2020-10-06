6b8bf124f4e58f9ead0f83032f8f418f023dcb7f
Gustavo Cândido <gustavocandidofranca@gmail.com>
2020-10-05 22:06:27 -0300

    first commit

---
+++ form.js
@@ -0,0 +1,36 @@
+
+const form = document.querySelector('form');
+console.log(form);
+
+
+form.addEventListener('change',(event)=>{
+    
+})
+
+const inputs = document.querySelectorAll('.register-box .content .form .input-group input');
+console.log(inputs);
+
+inputs.forEach((input)=>{
+input.addEventListener('focus',handleFocusInput);
+input.addEventListener('blur',handleBlurInput);
+
+})
+function handleBlurInput(event){
+    const input = event.target;
+    const label = input.closest('.input-group').querySelector('label');
+
+    if(input.value.length > 0){
+        return;
+    }
+
+    label.classList.remove('is-focused');
+
+}
+function handleFocusInput(event){
+   const input = event.target;
+
+   const label = input.closest('.input-group').querySelector('label');
+
+
+   label.classList.add('is-focused');
+}
